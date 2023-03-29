f33e0b1a01e52d5535d39b42862d98b5ca135092
Mário Nogueira <71707492+MarioNogueira01@users.noreply.github.com>
2023-03-29 15:20:53 +0100

    Criação do diretório

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.lang.reflect.*;
+import java.util.*;
+public class Main {
+    public static String capital(String s) {
+        String res = s.toLowerCase();
+        Character initial = Character.toUpperCase(res.charAt(0));
+        StringBuilder sb = new StringBuilder(res);
+        sb.setCharAt(0, initial);
+        final String answer = sb.toString();
+        return answer;
+    }
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner (System.in);
+
+        int [] line = readLines.readLine(sc.nextLine());
+        
+        Constructor<?> constructor;
+        Class<?> cl;
+        FiguraGeometrica f;
+        String s;
+        String [] aos;
+        while (sc.hasNextLine()) {
+            s = sc.nextLine();
+            aos = s.split(" ");
+            try {
+                cl = Class.forName(capital(aos[0]));
+                constructor = cl.getConstructor (new Class[] {String.class});
+                f = (FiguraGeometrica) constructor.newInstance(s);
+                System.out.println(f);
+                // Your code here: guarde aqui o seu obstaculo
+            }
+            catch (Exception e) {
+                System.out.println("Tipo de obstaculo desconhecido");
+            }
+        }
+        sc.close();
+    }
+}
